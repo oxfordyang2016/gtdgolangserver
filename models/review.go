4b85bfc4e38db00f63e087b9f3525d7aeb245f78
yangming <yang756260386@gmail.com>
2019-04-15 09:11:12 +0800

    modify the bug of reviewofdays

--- models/review.go
+++ models/review.go
@@ -117,12 +117,20 @@ func Reviewalgorithmjson(c *gin.Context) {
   var reviewdays []Reviewofday
   db.Where("email =  ?", email).Order("date").Find(&reviewdays)
   //if u set the len,u will get the size of slice
-  reviewdays = reviewdays[len(reviewdays)-31:]
-
-  c.JSON(200, gin.H{
+  if len(reviewdays)<33{
+    c.JSON(200, gin.H{
       //"reviewdata":review30days,
       "reviewdata":reviewdays,
     })
+  }else{
+    reviewdays = reviewdays[len(reviewdays)-31:]
+  
+    c.JSON(200, gin.H{
+      //"reviewdata":review30days,
+      "reviewdata":reviewdays,
+    })
+  }
+ 
 
 }
 
