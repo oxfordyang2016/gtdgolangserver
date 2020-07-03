d09b805fd1885650a2052ee88c0be473ef34ebde
yangming <yang756260386@gmail.com>
2020-07-04 01:27:29 +0800

    add push notification request part

--- models/schedule.go
+++ models/schedule.go
@@ -1,24 +1,41 @@
 
 package models
-// import(
-// "fmt"
+import(
+"fmt"
+"bytes"
 // "time"
 // "sort"
-// //"github.com/bradfitz/slice"
-// //"encoding/json"
-// "net/http"
+//"github.com/bradfitz/slice"
+"encoding/json"
+"net/http"
 // "github.com/fatih/color"
 // "github.com/jinzhu/gorm"
 // //"strconv"
 // //"github.com/jinzhu/gorm"
 // "github.com/gin-gonic/gin"
-// //"github.com/gin-contrib/sessions"
-// //_ "github.com/jinzhu/gorm/dialects/mysql"
-// _ "github.com/jinzhu/gorm/dialects/postgres"
-// _ "github.com/lib/pq"
+//"github.com/gin-contrib/sessions"
+//_ "github.com/jinzhu/gorm/dialects/mysql"
+_ "github.com/jinzhu/gorm/dialects/postgres"
+_ "github.com/lib/pq"
 // "github.com/tidwall/gjson"
-// //"github.com/tidwall/gjson"
-// )
+//"github.com/tidwall/gjson"
+)
+
+func push2scheduler(url string,taskid string,jobid string,task string ,time [6]int){
+values := map[string]interface{}{"taskid":taskid, "task":task,"time":time,"jobid":jobid }
+jsonValue, _ := json.Marshal(values)
+resp, _ := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
+if (resp!=nil){
+	fmt.Println("ok")
+}
+// fmt.Println(resp)
+}
+
+
+
+
+
+
 
 // type(
 // Goals  struct{
