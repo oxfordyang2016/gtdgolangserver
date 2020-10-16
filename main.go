050fbf65bdcd34fabe37515d46189e13bfec6fef
yang756260386@gmail.com <yang756260386@gmail.com>
2020-10-16 08:17:13 +0800

    project version

--- main.go
+++ main.go
@@ -8,25 +8,21 @@ package main
 //please attention use  the things u donnot famialr with
 //gopath https://github.com/golang/go/wiki/SettingGOPATH
 
-
 /*database*/
 //in order to keep db from losing ,i using db backup https://www.eversql.com/how-to-transfer-a-mysql-database-between-two-servers/
-
-
-
-
 
 import (
 	//"net/http"
 	//"github.com/yangming/stringutil"
-  //"fmt"
-  // "os"
-  // "io"
-  //"./math"
-  "github.com/gin-contrib/cors"
+	//"fmt"
+	// "os"
+	// "io"
+	//"./math"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-  //"github.com/gin-contrib/sessions"
+
+	//"github.com/gin-contrib/sessions"
 	// _ "github.com/jinzhu/gorm/dialects/postgres"
 	// the  . https://www.golang-book.com/books/intro/11
 	//_ "github.com/jinzhu/gorm/dialects/sqlite"
@@ -34,136 +30,127 @@ import (
 )
 
 var db *gorm.DB
-//about init https://stackoverflow.com/questions/24790175/when-is-the-init-function-run
 
 //about init https://stackoverflow.com/questions/24790175/when-is-the-init-function-run
 
-
-
+//about init https://stackoverflow.com/questions/24790175/when-is-the-init-function-run
 
 func main() {
 	//golang https://www.goinggo.net/2014/03/exportedunexported-identifiers-in-go.html
 	//golang import var
 	//fmt.Println(Modeltest)
 
-  // f, err := os.Create("/tmp/dat2")
+	// f, err := os.Create("/tmp/dat2")
 
-// f, _ := os.Create("engine.log")
-// gin.DefaultWriter = io.MultiWriter(f)
-router := gin.Default()
-router.Use(cors.Default())
-v1 := router.Group("/v1")
+	// f, _ := os.Create("engine.log")
+	// gin.DefaultWriter = io.MultiWriter(f)
+	router := gin.Default()
+	router.Use(cors.Default())
+	v1 := router.Group("/v1")
 
-
-  router.LoadHTMLGlob("templates/*.html")
+	router.LoadHTMLGlob("templates/*.html")
 	//i using https://gitlab.com/gin-gonic/gin/issues/358 to server static css
-v1.StaticFile("/show.css", "./static/css/show.css")
-v1.StaticFile("/user.css", "./static/css/user.css")
-v1.StaticFile("/taskmanage.css", "./static/css/taskmanage.css")
-v1.StaticFile("/moment.js", "./static/js/moment.js")
-v1.StaticFile("/tasks.js", "./static/js/tasks.js")
+	v1.StaticFile("/show.css", "./static/css/show.css")
+	v1.StaticFile("/user.css", "./static/css/user.css")
+	v1.StaticFile("/taskmanage.css", "./static/css/taskmanage.css")
+	v1.StaticFile("/moment.js", "./static/js/moment.js")
+	v1.StaticFile("/tasks.js", "./static/js/tasks.js")
 
+	v1.StaticFile("/echart.js", "./static/js/echart.js")
+	v1.StaticFile("/finance.js", "./static/js/finance.js")
+	v1.StaticFile("/jquery.js", "./static/js/jquery.js")
+	v1.StaticFile("/style.css", "./static/css/style.css")
+	v1.StaticFile("/index.js", "./static/js/index.js")
+	v1.StaticFile("/background.png", "./static/images/background.png")
 
-v1.StaticFile("/echart.js", "./static/js/echart.js")
-v1.StaticFile("/finance.js", "./static/js/finance.js")
-v1.StaticFile("/jquery.js", "./static/js/jquery.js")
-v1.StaticFile("/style.css", "./static/css/style.css")
- v1.StaticFile("/index.js", "./static/js/index.js")
-v1.StaticFile("/background.png", "./static/images/background.png")
-
-	 //test api
-  v1.GET("/test", Test)
-   v1.GET("/time",Clock)
+	//test api
+	v1.GET("/test", Test)
+	v1.GET("/time", Clock)
 	//user system
-  v1.GET("/", User)
-	v1.POST("/login",Login)
-	v1.POST("/register",Register)
+	v1.GET("/", User)
+	v1.POST("/login", Login)
+	v1.POST("/register", Register)
 
+	//operate system command
+	//for blog update
+	v1.GET("/blogupdate", Blogupdate)
 
+	//json API
+	v1.GET("/inboxjson", Inboxjson)
+	v1.GET("/todayjson", Todaytaskjson)
+	v1.GET("/taskmanager", Taskmanager)
+	v1.GET("/yesterdayjson", Yesterdaytaskjson)
+	v1.GET("/tomorrowjson", Tomorrowtaskjson)
+	v1.GET("/unfinishedtasksjson", Unfinishedtaskjson)
+	v1.GET("/readinglist", Readinglistjson)
+	v1.GET("/habitlist", Habitlistjson)
+	v1.GET("/healthlist", Healthlistjson)
+	v1.GET("/financelist", Financelistjson)
+	v1.GET("/projectsjson", Projectsjson)
+	// 这里是评价组
+	v1.GET("/reviewsjson", Reviewsjson)
+	v1.GET("/reviewdaydatajson", Reviewalgorithmjson)
+	v1.GET("/reviewdaydatajsonforyangming", Reviewalgorithmjsonforyangming)
+	v1.GET("/reviewdaydatajsonforios", Reviewalgorithmjsonforios)
+	// 这里是获取今天的分数
+	v1.GET("/reviewscoreoftoday", Reviewscore_today)
 
-  //operate system command 
-  //for blog update
-  v1.GET("/blogupdate", Blogupdate) 
+	//tasks system
+	v1.POST("/createtask", CreatetaskbyJSON)
+	//v1.POST("/gtdcli",Createtask)
+	v1.POST("/createtaskfromsiri", Createtaskfromsiri)
+	//v1.POST("/gtdclifromios",Createtaskfromios)
+	v1.POST("/update", Update)
+	v1.POST("/giveuptasksbatch", Giveuptasksbatch)
+	v1.POST("/tomorrowtasksbatch", Tomorrowtasksbatch)
+	v1.POST("/updateforios", Updateforios)
+	v1.GET("/map", Googlemapservice)
+	v1.GET("/location", Canvas)
+	v1.GET("/deadlinesystemjson", Deadlinesystem)
+	v1.POST("/taskexecute", Taskexecutelogfun)
+	//principle system
+	v1.POST("/createprinciplelib", Createprinciplelib)
+	v1.POST("/createprincipledetail", Createprincipledetail)
+	v1.POST("/connectpcodewithtasktag", Connectpcodewithtasktag)
+	// v1.POST("/Createprincipledetail",Createprincipledetail)
+	v1.GET("/getprinciple", Principlesystem)
+	v1.GET("/getprincipledetails", Principlesdetailsystem)
+	v1.GET("/searchwithprinciplecode", Searchwithprinciplecode)
 
-  //json API
-  v1.GET("/inboxjson",Inboxjson)
-  v1.GET("/todayjson",Todaytaskjson)
-  v1.GET("/taskmanager",Taskmanager)
-  v1.GET("/yesterdayjson",Yesterdaytaskjson)
-  v1.GET("/tomorrowjson",Tomorrowtaskjson)
-  v1.GET("/unfinishedtasksjson",Unfinishedtaskjson)
-  v1.GET("/readinglist",Readinglistjson) 
-  v1.GET("/habitlist",Habitlistjson)
-  v1.GET("/healthlist",Healthlistjson)
-  v1.GET("/financelist",Financelistjson)
-  v1.GET("/projectsjson",Projectsjson)
-  // 这里是评价组
-  v1.GET("/reviewsjson",Reviewsjson)
-  v1.GET("/reviewdaydatajson", Reviewalgorithmjson)  
-  v1.GET("/reviewdaydatajsonforyangming", Reviewalgorithmjsonforyangming)  
-  v1.GET("/reviewdaydatajsonforios", Reviewalgorithmjsonforios)
-  // 这里是获取今天的分数
-  v1.GET("/reviewscoreoftoday", Reviewscore_today)
-  
+	//goalsystem
+	v1.POST("/createproject", Createprojectofgoal)
+	v1.POST("/updateproject", Updateprojectofgoal)
+	v1.POST("/creategoal", Creategoal)
+	v1.POST("/updategoal", Updategoal)
+	v1.GET("/comparegoal", Goalcompare)
+	v1.GET("/searchwithgoalcode", Searchwithgoalcode)
+	v1.GET("/goaljson", Goalsystem)
+	v1.GET("/goalsgraph", Goalsjson)
+	v1.GET("/goallist", Goallist)
+	v1.GET("/goalincludeproject", Goalincludeproject)
+	//web page
+	//v1.GET("/mainboard",Mainboard)
+	v1.GET("/inbox", Inbox)
+	v1.GET("/project", Project)
+	v1.GET("/everyday", Everydays)
+	v1.GET("/pride", Finished)
+	v1.GET("/place", Placebased)
+	v1.GET("/freewriting", Freewriting)
+	v1.GET("/todaytasks", Todaytaskweb)
+	//get the tree map
+	v1.GET("/review", Review)
+	v1.GET("/reviewfrommaster", Reviewfromyangming)
+	v1.GET("/reviewfortimes", Reviewforstastics)
+	v1.GET("/errorlogjson", Errorlog)
+	v1.GET("/problemsjson", Problemssystem)
+	v1.GET("/questionsjson", Questionssystem)
 
-
-
-
-        //tasks system
-  v1.POST("/createtask",CreatetaskbyJSON)
-  //v1.POST("/gtdcli",Createtask)
-  v1.POST("/createtaskfromsiri",Createtaskfromsiri) 
-        //v1.POST("/gtdclifromios",Createtaskfromios)
-  v1.POST("/update",Update)
-  v1.POST("/giveuptasksbatch",Giveuptasksbatch)
-  v1.POST("/tomorrowtasksbatch",Tomorrowtasksbatch)
-  v1.POST("/updateforios",Updateforios)	
-  v1.GET("/map",Googlemapservice)
-  v1.GET("/location",Canvas)
-  v1.GET("/deadlinesystemjson",Deadlinesystem) 
-  v1.POST("/taskexecute",Taskexecutelogfun)
-  //principle system
-  v1.POST("/createprinciplelib",Createprinciplelib)
-  v1.POST("/createprincipledetail",Createprincipledetail) 
-  v1.POST("/connectpcodewithtasktag",Connectpcodewithtasktag) 
-  // v1.POST("/Createprincipledetail",Createprincipledetail)
-  v1.GET("/getprinciple",Principlesystem)
-  v1.GET("/getprincipledetails",Principlesdetailsystem)
-  v1.GET("/searchwithprinciplecode",Searchwithprinciplecode)
-
-         //goalsystem
-
-  v1.POST("/creategoal",Creategoal)
-  v1.POST("/updategoal",Updategoal)
-  v1.GET("/comparegoal",Goalcompare)    
-  v1.GET("/searchwithgoalcode",Searchwithgoalcode)  
-  v1.GET("/goaljson",Goalsystem)
-  v1.GET("/goalsgraph",Goalsjson)
-  v1.GET("/goallist",Goallist)
-  v1.GET("/goalincludeproject",Goalincludeproject)
- //web page
- //v1.GET("/mainboard",Mainboard)
-        v1.GET("/inbox",Inbox)
-        v1.GET("/project",Project)
-        v1.GET("/everyday",Everydays)
-        v1.GET("/pride",Finished)
-        v1.GET("/place",Placebased)
-        v1.GET("/freewriting",Freewriting)
-        v1.GET("/todaytasks",Todaytaskweb)
-        //get the tree map
-        v1.GET("/review",Review)
-        v1.GET("/reviewfrommaster",Reviewfromyangming)
-        v1.GET("/reviewfortimes",Reviewforstastics)
-        v1.GET("/errorlogjson",Errorlog)
-        v1.GET("/problemsjson",Problemssystem)
-        v1.GET("/questionsjson",Questionssystem)
-    
-        v1.GET("/reviewgraphforios",Reviewforios)
-        v1.GET("/search",Search)
-        v1.GET("/searchwithtag",Searchwithtags)
-        //finance
-        v1.GET("/mybalancejson",Getmywealth)
-        //v1.POST("/createfee",CreatefeebyJSON)
-        router.Run(":8081")
+	v1.GET("/reviewgraphforios", Reviewforios)
+	v1.GET("/search", Search)
+	v1.GET("/searchwithtag", Searchwithtags)
+	//finance
+	v1.GET("/mybalancejson", Getmywealth)
+	//v1.POST("/createfee",CreatefeebyJSON)
+	router.Run(":8081")
 
 }
--- models/project.go
+++ models/project.go
@@ -1,133 +1,128 @@
 package models
 
 import (
-"fmt"
-// "github.com/fatih/color"
-// "encoding/json"
-// "net/http"
-// "github.com/jinzhu/gorm"
-// "strconv"
-"time"
-// // "math"
-"github.com/jinzhu/gorm"
-"github.com/gin-gonic/gin"
-// //"github.com/gin-contrib/sessions"
-// //_ "github.com/jinzhu/gorm/dialects/mysql"
-// _ "github.com/jinzhu/gorm/dialects/postgres"
-// _ "github.com/lib/pq"
-// "github.com/bradfitz/slice"
-// "math"
-"github.com/tidwall/gjson"
-// "github.com/gomodule/redigo/redis"
+	"fmt"
+	// "github.com/fatih/color"
+	// "encoding/json"
+	// "net/http"
+	// "github.com/jinzhu/gorm"
+	// "strconv"
+	"time"
+	// // "math"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+
+	// //"github.com/gin-contrib/sessions"
+	// //_ "github.com/jinzhu/gorm/dialects/mysql"
+	// _ "github.com/jinzhu/gorm/dialects/postgres"
+	// _ "github.com/lib/pq"
+	// "github.com/bradfitz/slice"
+	// "math"
+	"github.com/tidwall/gjson"
+	// "github.com/gomodule/redigo/redis"
 )
 
-
-
-
-type Projectofgoals  struct {
+type Projectofgoals struct {
 	gorm.Model
-	Createtime string   `json:"createtime"`
-	Goal    string   `json:"goal"`     
-	Project    string   `json:"project"`
-	Status    string   `json:"status"`
-	Email    string   `json:"email"`
-	Marktime    string   `json:"marktime"`
+	Createtime string `json:"createtime" gorm:"default:'unsepcified'"`
+	Goal       string `json:"goal"`
+	Goalcode   string `json:"goalcode"`
+	Project    string `json:"project"`
+	Status     string `json:"status"`
+	Email      string `json:"email"`
+	// Sarttime   string `json:"starttime" gorm:"default:'unspecified'"`
+	Endtime string `json:"endtime" gorm:"default:'unspecified'"`
 	// Details string `json:"details" sql:"type:text;"`
- }
+}
 
-
-
-
-
- func Createprojectofgoal(c *gin.Context) {
+func Createprojectofgoal(c *gin.Context) {
 	buf := make([]byte, 1024)
 	num, _ := c.Request.Body.Read(buf)
 	reqBody := string(buf[0:num])
-	emailcookie,_:=c.Request.Cookie("email")
+	emailcookie, _ := c.Request.Cookie("email")
 	fmt.Println(emailcookie.Value)
-	email:=emailcookie.Value
-	goalname := gjson.Get(reqBody, "goal").String()
+	email := emailcookie.Value
+	goalcode := gjson.Get(reqBody, "goalcode").String()
+	var goalforquery Goalfordbs
+	db.Where(&Goalfordbs{Goalcode: goalcode, Email: email}).First(&goalforquery)
+	goalname := goalforquery.Name
 	projectname := gjson.Get(reqBody, "projectname").String()
 	// status := gjson.Get(reqBody, "status").String()
 	// projectstatus := "unspecified"
 	// marktime  :=  "unspecified"
-// 	if status=="unspecified"||status==""{
-// 		projectstatus = "unspecified"
-// 	}
-// 	if status == "finished"{
-// 		projectstatus = "finished"
-// 		loc, _ := time.LoadLocation("Asia/Shanghai") 
-//    marktime =  time.Now().In(loc).AddDate(0, 0, 1).Format("060102")
-// 	}
+	// 	if status=="unspecified"||status==""{
+	// 		projectstatus = "unspecified"
+	// 	}
+	// 	if status == "finished"{
+	// 		projectstatus = "finished"
+	// 		loc, _ := time.LoadLocation("Asia/Shanghai")
+	//    marktime =  time.Now().In(loc).AddDate(0, 0, 1).Format("060102")
+	// 	}
 	//这里原来引号出了问题。。。。多出了一个空格
-	loc, _ := time.LoadLocation("Asia/Shanghai") 
-	createtime :=  time.Now().In(loc).AddDate(0, 0, 1).Format("060102")
+	loc, _ := time.LoadLocation("Asia/Shanghai")
+	createtime := time.Now().In(loc).AddDate(0, 0, 1).Format("060102")
 	var countofproject int
-	db.Where(&Projectofgoals{Goal:goalname, Project: projectname,Email:email}).Count(&countofproject)
-	if countofproject >0{
+	var project Projectofgoals
+	db.Where("Email= ?", email).Where("Project=?", projectname).Where("Goalcode=?", goalcode).Find(&project).Count(&countofproject)
+	// db.Where(&Projectofgoals{Goalcode: goalcode, Project: projectname, Email: email}).Count(&countofproject)
+	if countofproject > 0 {
 		c.JSON(200, gin.H{
-			"result":"u have created project",
-		  })
-		  return
+			"result": "u have created project",
+		})
+		return
 	}
-	projectofgoal := Projectofgoals{Createtime:createtime,Goal:goalname,Project:projectname,Email:email,Status:"unspecified",Marktime:"unspecified"}
+	projectofgoal := Projectofgoals{Createtime: createtime, Goalcode: goalcode, Goal: goalname, Project: projectname, Email: email, Status: "unspecified"}
 	db.Create(&projectofgoal).Scan(&projectofgoal)
 	c.JSON(200, gin.H{
-		"result":"u have created project",
-	  })
+		"result": " created project successfully",
+	})
 }
-
-
-
 
 func Updateprojectofgoal(c *gin.Context) {
 	buf := make([]byte, 1024)
 	num, _ := c.Request.Body.Read(buf)
 	reqBody := string(buf[0:num])
-	emailcookie,_:=c.Request.Cookie("email")
+	emailcookie, _ := c.Request.Cookie("email")
 	fmt.Println(emailcookie.Value)
-	email:=emailcookie.Value
-	goalname := gjson.Get(reqBody, "goal").String()
+	email := emailcookie.Value
+	// goalname := gjson.Get(reqBody, "goal").String()
+	goalcode := gjson.Get(reqBody, "goalcode").String()
+	fmt.Println(goalcode)
 	projectname := gjson.Get(reqBody, "projectname").String()
 	status := gjson.Get(reqBody, "status").String()
 	projectstatus := "unspecified"
-	marktime  :=  "unspecified"
-	if status=="unspecified"||status==""{
+	endtime := "unspecified"
+	if status == "unspecified" || status == "" {
 		projectstatus = "unspecified"
 	}
-	if status == "finished"{
+	if status == "finished" {
 		projectstatus = "finished"
-		loc, _ := time.LoadLocation("Asia/Shanghai") 
-   marktime =  time.Now().In(loc).AddDate(0, 0, 1).Format("060102")
+		loc, _ := time.LoadLocation("Asia/Shanghai")
+		endtime = time.Now().In(loc).AddDate(0, 0, 1).Format("060102")
 	}
 
-	if status == "giveup"{
+	if status == "giveup" {
 		projectstatus = "giveup"
-		loc, _ := time.LoadLocation("Asia/Shanghai") 
-   marktime =  time.Now().In(loc).AddDate(0, 0, 1).Format("060102")
+		loc, _ := time.LoadLocation("Asia/Shanghai")
+		endtime = time.Now().In(loc).AddDate(0, 0, 1).Format("060102")
 	}
 
-	  var  projectfromclient Projectofgoals
-	  db.Where(&Projectofgoals{Goal:goalname, Project: projectname,Email:email}).First(&projectfromclient)
+	var projectfromclient Projectofgoals
+	db.Where(&Projectofgoals{Goalcode: goalcode, Project: projectname, Email: email}).First(&projectfromclient)
 	//   if goalname != "unspecified"{
 	// 	projectfromclient.Goal = goalname
-	//   } 
-	  if projectname != "unspecified"{
-        projectfromclient.Project = projectname
-	  }  
+	//   }
+	if projectname != "unspecified" {
+		projectfromclient.Project = projectname
+	}
 
-	  
-	  if projectstatus != "unspecified"{
-		projectfromclient.Status  =projectstatus
-		projectfromclient.Marktime = marktime
-	  }
-	  
-	  db.Save(&projectfromclient)
+	if projectstatus != "unspecified" {
+		projectfromclient.Status = projectstatus
+		projectfromclient.Endtime = endtime
+	}
+
+	db.Save(&projectfromclient)
 	c.JSON(200, gin.H{
-		"result":"u have created project",
-	   })
+		"result": "u have update project",
+	})
 }
-
-
-
-
